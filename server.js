2a11e0563f9bcb1449537a7673ea03a5ff7751b0
Ryan Logan <ryan_plogan@yahoo.com>
2021-10-14 20:39:35 -0700

    finished for heroku

--- server.js
+++ server.js
@@ -1,38 +1,3 @@
-// const path = require('path');
-// const express = require('express');
-// const sequelize = require('./config/connection');
-// const routes = require('./controllers');
-// const exphbs = require('express-handlebars');
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({helpers})
-// const session = require('express-session');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {expire: 60000},
-//   resave: true,
-//   rolling: true,
-//   saveUninitialized: false,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
-
-// app.use(session(sess));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-// app.use(routes);
-// sequelize.sync({ force: false }).then(() => {
-//     app.listen(PORT, () => console.log('Now listening'));
-// }); 
 
 const express = require('express');
 const routes = require('./controllers');
